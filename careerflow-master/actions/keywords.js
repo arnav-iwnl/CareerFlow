abd88e8348fa6bab71cfd8435c8b0a8813dbec84
Apoorva S <129332224+engg-apoorvasaxena@users.noreply.github.com>
2025-03-28 18:49:04 +0530

    Update keywords.js

--- careerflow-master/actions/keywords.js
+++ careerflow-master/actions/keywords.js
@@ -1,20 +1,26 @@
 "use server";
-import { GoogleGenerativeAI } from "@google/generative-ai";
-import { auth } from "@clerk/nextjs/server";
+
 import { db } from "@/lib/prisma";
+import { auth } from "@clerk/nextjs/server";
+import { GoogleGenerativeAI } from "@google/generative-ai";
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
 export async function suggestKeywords({ jobDescription }) {
-  const { userId } = auth();
+  const { userId } = await auth();
+  // console.log(userId);
   if (!userId) throw new Error("Unauthorized");
 
   const user = await db.user.findUnique({
     where: { clerkUserId: userId },
+    select: {
+      industry: true
+    }
   });
   if (!user) throw new Error("User not found");
+  // const user.industry = 'WEB DEV';
 
-  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
   const prompt = `
     Given the following job description and user industry, extract relevant keywords (skills, tools, certifications, etc.) that should be included in a resume to make it ATS-friendly. Return the keywords as a comma-separated list.
 
@@ -25,3 +31,5 @@ export async function suggestKeywords({ jobDescription }) {
   const keywords = result.response.text().trim();
   return keywords;
 }
+
+
